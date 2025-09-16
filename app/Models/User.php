4c1a40669e8c292fb79abf0901d0a7ffd15a2696
MoisesCorcho <mcorchoperez@gmail.com>
2025-09-16 13:19:07 -0500

    feat: agregar modelo Organization y su respectiva migración

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use App\Enums\DniTypeEnum;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class User extends Authenticatable
 {
@@ -65,5 +66,10 @@ class User extends Authenticatable
                 return $this->name . ' ' . $this->last_name;
             },
         );
+    }
+
+    public function organizations(): HasMany
+    {
+        return $this->hasMany(Organization::class);
     }
 }
